ca809a16088df29ed4a4ed88555b66e04226cc69
korbutds <sys@hohou.ru>
2020-11-17 18:58:21 +0300

    Добавил модули trip-info и trip-controls. Закончил рендер header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-'use strict';
+import {createAboutTripTemplate} from "./view/trip-info.js";
+import {createTripControlsTemplate} from "./view/trip-controls.js";
 
-// container = место куда вставляем разметку; content = text разметки; position = определяет позицию добавляемого элемента относительно элемента, вызвавшего метод. =
 const render = (container, content, position) => {
   container.insertAdjacentHTML(position, content);
-}
+};
+
+const pageHeader = document.querySelector(`.page-header`);
+const tripMainElement = pageHeader.querySelector(`.trip-main`);
+const tripControlsElement = pageHeader.querySelector(`.trip-main__trip-controls`);
+
+render(tripMainElement, createAboutTripTemplate(), `afterbegin`);
+render(tripControlsElement, createTripControlsTemplate(), `afterbegin`);
