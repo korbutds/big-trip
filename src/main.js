72a22759299e71d504b931e26a9e2bbefd795603
korbutds <sys@hohou.ru>
2020-11-18 10:58:12 +0300

    Отрисовал страницу в js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import {createAboutTripTemplate} from "./view/trip-info.js";
-import {createTripControlsTemplate} from "./view/trip-controls.js";
+import {createTripFiltersTemplate} from "./view/trip-filters.js";
+import {createTripSortTemplate} from "./view/trip-sort.js";
+import {createTripListTemplate} from "./view/trip-list.js";
+import {creatNewPointTemplate} from "./view/trip-new-point.js";
+import {createEditPointTemplate} from "./view/trip-edit-point.js";
+import {createTripPointTemplate} from "./view/trip-point.js";
 
 const render = (container, content, position) => {
   container.insertAdjacentHTML(position, content);
@@ -8,6 +13,18 @@ const render = (container, content, position) => {
 const pageHeader = document.querySelector(`.page-header`);
 const tripMainElement = pageHeader.querySelector(`.trip-main`);
 const tripControlsElement = pageHeader.querySelector(`.trip-main__trip-controls`);
+const pageMain = document.querySelector(`.page-body__page-main`);
+const tripEventsSection = pageMain.querySelector(`.trip-events`);
 
 render(tripMainElement, createAboutTripTemplate(), `afterbegin`);
-render(tripControlsElement, createTripControlsTemplate(), `afterbegin`);
+render(tripControlsElement, createTripFiltersTemplate(), `afterbegin`);
+render(tripEventsSection, createTripSortTemplate(), `beforeend`);
+render(tripEventsSection, createTripListTemplate(), `beforeend`);
+
+const tripList = tripEventsSection.querySelector(`.trip-events__list`);
+render(tripList, creatNewPointTemplate(), `beforeend`);
+render(tripList, createEditPointTemplate(), `beforeend`);
+render(tripList, createTripPointTemplate(), `beforeend`);
+render(tripList, createTripPointTemplate(), `beforeend`);
+render(tripList, createTripPointTemplate(), `beforeend`);
+
--- src/view/trip-filters.js
+++ src/view/trip-filters.js
@@ -1,4 +1,4 @@
-export const createTripControlsTemplate = () => {
+export const createTripFiltersTemplate = () => {
   return `<div>
   <h2 class="visually-hidden">Switch trip view</h2>
   <nav class="trip-controls__trip-tabs  trip-tabs">
