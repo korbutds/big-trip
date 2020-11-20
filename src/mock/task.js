a395ccf1e8dbb2f6737336a5f2dd817f070df540
korbutds <sys@hohou.ru>
2020-11-20 17:41:13 +0300

    2.1 Описываем структуру данных задачи

---
+++ src/mock/task.js
@@ -0,0 +1,105 @@
+const FISH_TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+
+const getRandomFishText = (sentenseCount) => {
+  const fishTextArr = FISH_TEXT.split(`. `);
+  const fishTextLength = fishTextArr.length;
+  let sentenseArr = [];
+
+  for (let i = 0; i < sentenseCount; i++) {
+    sentenseArr.push(fishTextArr[getRandomInt(0, fishTextLength - 1)]);
+  }
+  return sentenseArr.join(`. `);
+};
+
+const getRandomArrayElement = (arr) => {
+  return arr[getRandomInt(0, arr.length - 1)];
+};
+
+const getRandomArray = (count, arr) => {
+  let pointArr = [];
+  const arrLength = arr.length;
+  for (let i = 0; i < count; i++) {
+    pointArr.push(arr[getRandomInt(0, arrLength - 1)]);
+  }
+  return pointArr;
+};
+
+const ROUTE_POINT_TYPES = [
+  `Taxi`,
+  `Bus`,
+  `Train`,
+  `Ship`,
+  `Transport`,
+  `Drive`,
+  `Flight`,
+  `Check-in`,
+  `Sightseeng`,
+  `Restaurant`
+];
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+// const SENTENSE_COUNT = 5;
+
+const DESTINATIONS = [
+  `San_Francisco`,
+  `Los-Angeles`,
+  `New-York`,
+  `Kokkari restaurante`,
+  `Hollywood Hills`
+];
+
+const OFFERS_LIST = {
+  'Order Uber': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Add luggage': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Switch to comfort': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Rent a car': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Add breakfast': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Book tickets': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+  'Lunch in city': {
+    'destination list': Array.from(new Set(getRandomArray(getRandomInt(0, 5), DESTINATIONS))),
+    'price': getRandomInt(20, 95)},
+};
+
+const createPhotosArr = () => {
+  const count = getRandomInt(1, 4);
+  const src = [];
+  for (let i = 0; i < count; i++) {
+    src.push(`http://picsum.photos/248/152?r=${Math.random()}`);
+  }
+
+  return {count, src};
+};
+
+const generateTask = () => {
+  return {
+    pointType: getRandomArrayElement(ROUTE_POINT_TYPES),
+    pointDestination: getRandomArrayElement(DESTINATIONS),
+    getPointOffers() {
+      const distanation = this.pointDestination;
+      return Object.keys(OFFERS_LIST).filter((value) => {
+        return OFFERS_LIST[value][`destination list`].includes(distanation);
+      });
+    },
+    description: getRandomFishText(getRandomInt(1, 5)),
+    photo: createPhotosArr()
+  };
+};
+const dfds = generateTask();
+console.log(dfds);
