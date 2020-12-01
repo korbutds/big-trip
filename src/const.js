02e7464be0aa047f600d212d58ef2044e6623dce
korbutds <sys@hohou.ru>
2020-12-01 13:32:29 +0300

    fix: Изменил данные

--- src/const.js
+++ src/const.js
@@ -1,113 +1,102 @@
-import {getRandomInt, getRandomArray, getRandomFishText, createPhotosArr} from "./utils.js";
+import {getRandomInt, getRandomFishText, createPhotosArr} from "./utils.js";
 import dayjs from "dayjs";
 
 const OFFERS_LIST = {
   orderUber: {
     id: `order-uber`,
     name: `Order uber`,
-    price: getRandomInt(20, 95)
+    price: 20
   },
   addLuggage: {
     id: `add-luggage`,
     name: `Add luggage`,
-    price: getRandomInt(20, 95)
+    price: 15
   },
   switchToComfort: {
     id: `switch-to-comfort`,
     name: `Switch to comfort`,
-    price: getRandomInt(20, 95)
+    price: 100
   },
   rentACar: {
     id: `rent-a-car`,
     name: `Rent a car`,
-    price: getRandomInt(20, 95)
+    price: 150
   },
   addBreakfast: {
     id: `add-breakfast`,
     name: `Add breakfast`,
-    price: getRandomInt(20, 95)
+    price: 25
   },
   bookTickets: {
     id: `book-tickets`,
     name: `Book tickets`,
-    price: getRandomInt(20, 95)
+    price: 12
   },
   lunchInCity: {
     id: `lunch-in-city`,
     name: `Lunch in city`,
-    price: getRandomInt(20, 95)
+    price: 45
   },
-};
-
-const getRandomOfferList = () => {
-  const offersNames = getRandomArray(getRandomInt(0, 5), Object.keys(OFFERS_LIST));
-  return offersNames.map((elem) => {
-    return {
-      name: OFFERS_LIST[elem].name,
-      price: OFFERS_LIST[elem].price,
-      isChecked: false,
-    };
-  });
 };
 
 const ROUTE_POINT_TYPES = {
   taxi: {
     name: `Taxi`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.orderUber, OFFERS_LIST.switchToComfort],
     iconSrc: `./img/icons/taxi.png`,
     price: getRandomInt(2, 20) * 20,
   },
   bus: {
     name: `Bus`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets],
     iconSrc: `./img/icons/bus.png`,
     price: getRandomInt(2, 20) * 20,
   },
   train: {
     name: `Train`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets, OFFERS_LIST.switchToComfort],
     iconSrc: `./img/icons/train.png`,
     price: getRandomInt(2, 20) * 20,
   },
   ship: {
     name: `Ship`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets, OFFERS_LIST.switchToComfort],
     iconSrc: `./img/icons/ship.png`,
     price: getRandomInt(2, 20) * 20,
   },
   transport: {
     name: `Transport`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets],
     iconSrc: `./img/icons/transport.png`,
     price: getRandomInt(2, 20) * 20,
   },
   drive: {
     name: `Drive`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.rentACar],
     iconSrc: `./img/icons/drive.png`,
     price: getRandomInt(2, 20) * 20,
   },
   flight: {
     name: `Flight`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets, OFFERS_LIST.switchToComfort, OFFERS_LIST.addLuggage],
     iconSrc: `./img/icons/flight.png`,
     price: getRandomInt(2, 20) * 20,
   },
   checkIn: {
     name: `Check-in`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.lunchInCity],
     iconSrc: `./img/icons/check-in.png`,
     price: getRandomInt(2, 20) * 20,
   },
   sightseeng: {
     name: `Sightseeng`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.bookTickets, OFFERS_LIST.lunchInCity, OFFERS_LIST.addBreakfast],
     iconSrc: `./img/icons/sightseeing.png`,
     price: getRandomInt(2, 20) * 20,
   },
   restaurant: {
     name: `Restaurant`,
-    offers: getRandomOfferList(),
+    offers: [OFFERS_LIST.lunchInCity, OFFERS_LIST.switchToComfort],
     iconSrc: `./img/icons/restaurant.png`,
     price: getRandomInt(2, 20) * 20,
   },
